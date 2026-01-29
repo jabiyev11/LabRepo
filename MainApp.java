844c4f3aa6be17d9b4c85a174af4cfb79b87ed40
Cabiyev Cavid <Cavid.Cabiyev@kapitalbank.az>
2026-01-29 14:07:29 +0400

    Commited main app file

---
+++ MainApp.java
@@ -0,0 +1,10 @@
+package org.example.labGradle;
+
+public class MainApp {
+
+  public static void main(String[] args) {
+
+    System.out.println("Gradle build and Git workflow successful.");
+  }
+
+}
